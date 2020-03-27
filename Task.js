08d2df94b0adb22042299d968b29992e088d37aa
lightnk <36459043+lightnk@users.noreply.github.com>
2020-03-27 22:54:28 +0100

    Add files via upload
    I still need to learn more about JS

---
+++ Task.js
@@ -0,0 +1,14 @@
+let message;
+
+message = 'Light';
+
+message = 'Nkemjika';
+
+console.log(message);
+
+var i = ["HTML", "CSS", "JAVASCRIPT". "JAVA", "PYTHON"];
+
+var i;
+for (i = 0; i < courses.length; i++) {
+  HTML  += console.log[i] + "<br>";
+}
